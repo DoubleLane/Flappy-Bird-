28b992273c7734a702ef4f6baf73ee541c2473c8
Marko Vujošević <42012973+DoubleLane@users.noreply.github.com>
2024-06-12 11:07:47 +0200

    Update Base.py

--- Base.py
+++ Base.py
@@ -4,6 +4,8 @@ import os
 BASE_IMG=pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","base.png")))
 
 class Base:
+    #motion of base
+    
     VEL=5
     WIDTH=BASE_IMG.get_width()
     IMG=BASE_IMG
@@ -12,6 +14,7 @@ class Base:
         self.y=y
         self.x1=0
         self.x2=self.WIDTH
+        
     def move(self):
         self.x1-=self.VEL
         self.x2-=self.VEL
@@ -23,6 +26,9 @@ class Base:
             self.x2 = self.x1 +  self.WIDTH
 
     def draw(self,win):
+        #movement of the base is determined by 2 pictures of the base that move one after the other
+        #ending of the one is the start for the another
+        
         win.blit(self.IMG, (self.x1,self.y))
         win.blit(self.IMG, (self.x2,self.y))
        
